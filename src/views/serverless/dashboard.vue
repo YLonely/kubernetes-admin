63f054d41a570dc2bed6ce07ba9a211c4d580c6e
YLonely <loneybw@gmail.com>
2021-03-10 09:39:22 +0000

    feat(dashboard.vue functiondeploy.vue): add dashboard from grafana, finish function deploy page

--- src/views/serverless/dashboard.vue
+++ src/views/serverless/dashboard.vue
@@ -1,3 +1,56 @@
 <template>
-    
+  <div>
+    <el-row :gutter="20">
+      <el-col :span="12">
+        <iframe
+          src="http://192.168.150.128:31114/d-solo/PQW4NA8Mz/original-open-faas-dashboard?orgId=1&refresh=5s&panelId=1&from=now-1m&to=now"
+          frameborder="0"
+        ></iframe>
+      </el-col>
+      <el-col :span="12">
+        <iframe
+          src="http://192.168.150.128:31114/d-solo/PQW4NA8Mz/original-open-faas-dashboard?orgId=1&refresh=5s&panelId=2&from=now-1m&to=now"
+          frameborder="0"
+        ></iframe>
+      </el-col>
+    </el-row>
+    <el-row :gutter="20">
+      <el-col :span="12">
+        <iframe
+          src="http://192.168.150.128:31114/d-solo/PQW4NA8Mz/original-open-faas-dashboard?orgId=1&refresh=5s&panelId=7&from=now-1m&to=now"
+          frameborder="0"
+        ></iframe>
+      </el-col>
+      <el-col :span="12">
+        <iframe
+          src="http://192.168.150.128:31114/d-solo/PQW4NA8Mz/original-open-faas-dashboard?orgId=1&refresh=5s&panelId=8&from=now-1h"
+          frameborder="0"
+        ></iframe>
+      </el-col>
+    </el-row>
+    <el-row :gutter="20">
+      <el-row :gutter="20">
+        <el-col :span="12">
+          <iframe
+            src="http://192.168.150.128:31114/d-solo/PQW4NA8Mz/original-open-faas-dashboard?orgId=1&refresh=5s&panelId=3&from=now-1m&to=now"
+            frameborder="0"
+          ></iframe>
+        </el-col>
+        <el-col :span="12">
+          <iframe
+            src="http://192.168.150.128:31114/d-solo/PQW4NA8Mz/original-open-faas-dashboard?orgId=1&refresh=5s&panelId=4&from=now-1m&to=now"
+            frameborder="0"
+          ></iframe>
+        </el-col>
+      </el-row>
+    </el-row>
+  </div>
 </template>
+
+<style scoped>
+iframe {
+  height: 230px;
+  width: 100%;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.12), 0 0 6px rgba(0, 0, 0, 0.04);
+}
+</style>
--- src/views/serverless/functionDeploy.vue
+++ src/views/serverless/functionDeploy.vue
@@ -1,29 +1,24 @@
 <template>
   <el-tabs v-model="activeName" type="border-card">
     <el-tab-pane label="使用镜像部署" name="image">
-      <el-form ref="form" :model="form" label-width="80px">
+      <el-form ref="form" :model="form" label-width="80px" label-position="top">
         <el-form-item label="镜像名称:">
           <el-input v-model="form.imageName"></el-input>
         </el-form-item>
         <el-form-item label="函数名称:">
           <el-input v-model="form.funcName"></el-input>
         </el-form-item>
-        <el-form-item label="部署方式:">
-          <el-radio v-model="deployType" label="fast"
-            >快速部署(使用优化技术加速无服务器函数启动)</el-radio
-          >
-          <el-radio v-model="deployType" label="normal">普通部署</el-radio>
+        <el-form-item label="环境变量:" style="margin-bottom: 10px">
         </el-form-item>
-        <el-form-item label="环境变量:"> </el-form-item>
         <el-form-item v-for="(env, index) in form.envs" :key="index">
           <el-row :gutter="20" style="margin-top: -10px; margin-bottom: -10px">
-            <el-col :span="10">
+            <el-col :span="12">
               <el-input
                 placeholder="键:"
                 v-model="form.envs[index].key"
               ></el-input>
             </el-col>
-            <el-col :span="10">
+            <el-col :span="11">
               <el-input
                 placeholder="值:"
                 v-model="form.envs[index].value"
@@ -44,16 +39,17 @@
             </el-col>
           </el-row>
         </el-form-item>
-        <el-form-item label="Labels:"> </el-form-item>
+        <el-form-item label="Labels:" style="margin-bottom: 10px">
+        </el-form-item>
         <el-form-item v-for="(env, index) in form.labels" :key="index">
           <el-row :gutter="20" style="margin-top: -10px; margin-bottom: -10px">
-            <el-col :span="10">
+            <el-col :span="12">
               <el-input
                 placeholder="键:"
                 v-model="form.labels[index].key"
               ></el-input>
             </el-col>
-            <el-col :span="10">
+            <el-col :span="11">
               <el-input
                 placeholder="值:"
                 v-model="form.labels[index].value"
@@ -88,20 +84,162 @@
             全新部署<i class="el-icon-arrow-down el-icon--right"></i>
           </span>
           <el-dropdown-menu slot="dropdown">
-            <el-dropdown-item command="javascript">Javascript</el-dropdown-item>
-            <el-dropdown-item command="python">Python</el-dropdown-item>
-            <el-dropdown-item command="python-ml"
+            <el-dropdown-item command="text/javascript"
+              >Javascript</el-dropdown-item
+            >
+            <el-dropdown-item command="text/x-python">Python</el-dropdown-item>
+            <el-dropdown-item command="text/x-python-ml"
               >Python-Tensorflow</el-dropdown-item
             >
-            <el-dropdown-item command="golang">Golang</el-dropdown-item>
-            <el-dropdown-item command="java">Java</el-dropdown-item>
+            <el-dropdown-item command="text/x-go">Golang</el-dropdown-item>
+            <el-dropdown-item command="text/x-java">Java</el-dropdown-item>
           </el-dropdown-menu>
         </el-dropdown>
       </span>
-      <div v-if="codeType === 'python' || codeType === 'python-ml'">
-        <codemirror v-model="code" :options="cmOptions" />
+      <el-row> 函数语言：{{ readableCodeType }} </el-row>
+      <div
+        v-if="codeType === 'text/x-python' || codeType === 'text/x-python-ml'"
+      >
+        <el-row :gutter="10">
+          <el-col
+            style="
+              border: 1px solid Gainsboro;
+              margin-top: 20px;
+              margin-left: 85px;
+            "
+            :span="11"
+          >
+            <codemirror v-model="code" :options="cmOptions" />
+          </el-col>
+          <el-col :span="11">
+            <el-row> requirements.txt </el-row>
+            <el-row>
+              <el-input
+                type="textarea"
+                :autosize="{ minRows: 14, maxRows: 14 }"
+                resize="none"
+                placeholder="请输入依赖库"
+                v-model="requirements"
+                style="margin-top: 2px"
+              >
+              </el-input>
+            </el-row>
+          </el-col>
+        </el-row>
       </div>
-      <div v-else>else</div>
+      <div v-else>
+        <el-row>
+          <el-col
+            style="
+              border: 1px solid Gainsboro;
+              margin-top: 20px;
+              margin-left: 85px;
+            "
+            :span="21"
+          >
+            <codemirror v-model="code" :options="cmOptions" />
+          </el-col>
+        </el-row>
+      </div>
+      <hr style="color: Gainsboro; margin-top: 40px" />
+      <el-row style="margin-top: 20px">
+        <el-form
+          ref="form"
+          :model="form"
+          label-width="80px"
+          label-position="top"
+        >
+          <el-form-item label="镜像名称:">
+            <el-input
+              v-model="form.imageName"
+              placeholder="功能:版本，如 curl:v0"
+            >
+              <template slot="prepend">serverless.io/image/</template>
+            </el-input>
+          </el-form-item>
+          <el-form-item label="函数检查点名称:" label-width="auto">
+            <el-input :value="form.imageName" :readonly="true">
+              <template slot="prepend">serverless.io/checkpoint/</template>
+            </el-input>
+          </el-form-item>
+          <el-form-item label="函数名称:">
+            <el-input v-model="form.funcName"></el-input>
+          </el-form-item>
+          <el-form-item label="环境变量:" style="margin-bottom: 10px">
+          </el-form-item>
+          <el-form-item v-for="(env, index) in form.envs" :key="index">
+            <el-row
+              :gutter="20"
+              style="margin-top: -10px; margin-bottom: -10px"
+            >
+              <el-col :span="12">
+                <el-input
+                  placeholder="键:"
+                  v-model="form.envs[index].key"
+                ></el-input>
+              </el-col>
+              <el-col :span="11">
+                <el-input
+                  placeholder="值:"
+                  v-model="form.envs[index].value"
+                ></el-input>
+              </el-col>
+              <el-col :span="1">
+                <el-button
+                  :icon="
+                    index === form.envs.length - 1
+                      ? 'el-icon-circle-plus-outline'
+                      : 'el-icon-remove-outline'
+                  "
+                  circle
+                  size="mini"
+                  @click="handleEnv(index)"
+                >
+                </el-button>
+              </el-col>
+            </el-row>
+          </el-form-item>
+          <el-form-item label="Labels:" style="margin-bottom: 10px">
+          </el-form-item>
+          <el-form-item v-for="(env, index) in form.labels" :key="index">
+            <el-row
+              :gutter="20"
+              style="margin-top: -10px; margin-bottom: -10px"
+            >
+              <el-col :span="12">
+                <el-input
+                  placeholder="键:"
+                  v-model="form.labels[index].key"
+                ></el-input>
+              </el-col>
+              <el-col :span="11">
+                <el-input
+                  placeholder="值:"
+                  v-model="form.labels[index].value"
+                ></el-input>
+              </el-col>
+              <el-col :span="1">
+                <el-button
+                  :icon="
+                    index === form.labels.length - 1
+                      ? 'el-icon-circle-plus-outline'
+                      : 'el-icon-remove-outline'
+                  "
+                  circle
+                  size="mini"
+                  @click="handleLabel(index)"
+                >
+                </el-button>
+              </el-col>
+            </el-row>
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary" @click.prevent.native="createFromImage"
+              >立即创建</el-button
+            >
+          </el-form-item>
+        </el-form>
+      </el-row>
     </el-tab-pane>
   </el-tabs>
 </template>
@@ -110,9 +248,12 @@
 import { codemirror } from "vue-codemirror";
 import "codemirror/lib/codemirror.css";
 import "codemirror/mode/javascript/javascript.js";
+import "codemirror/mode/python/python.js";
+import "codemirror/mode/go/go.js";
+import "codemirror/mode/clike/clike.js";
 
 // import theme style
-import "codemirror/theme/base16-dark.css";
+import "codemirror/theme/idea.css";
 export default {
   components: {
     codemirror,
@@ -120,13 +261,15 @@ export default {
   data() {
     return {
       activeName: "image",
-      codeType: "javascript",
+      codeType: "text/javascript",
+      readableCodeType: "Javascript",
       deployType: "fast",
-      code: "hello",
+      code: "",
+      requirements: "",
       cmOptions: {
         tabSize: 4,
         mode: "text/javascript",
-        theme: "base16-dark",
+        theme: "idea",
         lineNumbers: true,
         line: true,
       },
@@ -152,6 +295,26 @@ export default {
     handleCodeType: function (type) {
       this.activeName = "code";
       this.codeType = type;
+      if (type === "text/x-python-ml") {
+        this.cmOptions.mode = "text/x-python";
+      } else {
+        this.cmOptions.mode = type;
+      }
+      switch (type) {
+        case "text/x-python":
+        case "text/x-python-ml":
+          this.readableCodeType = "Python";
+          break;
+        case "text/javascript":
+          this.readableCodeType = "Javascript";
+          break;
+        case "text/x-go":
+          this.readableCodeType = "Golang";
+          break;
+        case "text/x-java":
+          this.readableCodeType = "Java";
+          break;
+      }
     },
     createFromImage: function () {},
     handleEnv: function (index) {
